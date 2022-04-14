aee17dd92e4479bd865c5af657e23666685851ba
Guillaume Rodier <guillaume@fanprime.io>
2022-04-14 17:40:31 +0200

    2022-14-04 - Weborama test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <input v-model="profil" placeholder="modifiez-moi">
+      <button @click="validate">
+        Valider
+      </button>
+    </div>
+    <div v-if="userInfos != null">
+      <p>
+        Name: {{ userInfos.name }}<br/>
+        Created date: {{ userInfos.created_date }}<br/>
+        avatar: {{ userInfos.avatar }}<br/>
+      </p>
+      <h3> Repos list </h3>
+      <div v-for="(repo, index) in userInfos.list_repos" :key="index">
+        <p>
+          <b>Url Repo:</b> {{ repo.url }}<br/>
+          <b>Description:</b> {{ repo.description }}<br/>
+          <b>Created at:</b> {{ repo.created_at }}<br/>
+        </p>
+        <div v-if="repo.has_issues">
+          <p> test</p>
+          {{ getIssues(repo.issues_url, repo.open_issues_count) }}
+          <!-- if has issues, show title of more recent assigned user -->
+        </div>
+        <hr />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      profil: "",
+      userInfos: null,
+    };
+  },
+  methods: {
+    async validate() {
+      try {
+        let response = await axios.get(`https://api.github.com/users/${this.profil}`);
+
+        let repo_list = await axios.get(response.data.repos_url);
+  
+        this.userInfos = {
+          name: response.data.name,
+          created_date: response.data.created_at,
+          avatar: response.data.avatar_url,
+          list_repos: repo_list.data,
+        };
+      } catch(e) {
+        console.error(e);
+        this.userInfos = null;
+      }
+    },
+    getIssues(issues_url, open_issues_count) {
+      return issues_url.replace("{/number}", "/" + open_issues_count);
+    }
+  },
 }
 </script>
 
